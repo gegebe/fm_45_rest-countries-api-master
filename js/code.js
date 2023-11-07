e1df3d1e1f765ce9667f0288b27f58e19d0a8a36
gegebe <ggirbes@gmail.com>
2023-11-07 10:58:12 +0100

    aproximación

--- js/code.js
+++ js/code.js
@@ -1,15 +1,23 @@
+let countries;
+
+//1. OBTENER PAISES DE LA API
 async function getCountries(){
   let countriesData = await fetch("https://restcountries.com/v3.1/all");
-  //console.log(countriesData);
+  console.log(countriesData);
   
-  let convertedData = await countriesData.json();
-  //console.log(convertedData);
+  countries = await countriesData.json();
+  console.log(countries);
 
-  convertedData.forEach(country => {
+  updateCountries(countries);
+}
+//3. CREAR UNA FUNCIÓN QUE RECORRE Y ACTUALIZA EL DOM
+function updateCountries(countries){
+  countries.forEach(country => {
     printCountry(country);
   });
 }
 
+//2. CREAR UNA FUNCIÓN PARA PINTAR EL DOM
 function printCountry(country){
   let countriesBox = document.querySelector("#countries-selection-box");
 
@@ -31,30 +39,42 @@ function printCountry(country){
   countryRegion.textContent = country.region;
 }
 
+//4. LLAMAR LA PRIMERA FUNCIÓN
 getCountries();
 
-// let convertedData = [];
-
+// 5. SI BUSCAS
 let inputSearch = document.querySelector("#input-field-country");
+
 inputSearch.addEventListener("input", (event) => {
+
+  document.querySelector("#countries-selection-box").innerHTML = "";
 
   //Obtener valor
   let inputValue = event.target.value.toLowerCase();
 
   //Filtrar paises
-  let filteredCountries = convertedData.filter(
-    (country) => country.name.toLowerCase.includes(inputValue)
+  let filteredCountries = countries.filter(
+    (country) => country.name.common.toLowerCase().includes(inputValue)
     );
 
-  //Borrar DOM
-  countriesBox = document.querySelector("#countries-selection-box").innerHTML = "";
+    updateCountries(filteredCountries);
 
-
-
-  //Nostrar en el contenendor
-//   filteredCountries.forEach((country) => {
-//     printCountry(country);
-//   }):
-
-// })
 });
+
+// 6. SI SELECCIONAS
+let selectRegion = document.querySelector("#countrySelect");
+
+selectRegion.addEventListener("change", (event) => {
+  
+  document.querySelector("#countries-selection-box").innerHTML = "";
+
+  //Obtener valor
+  let selectValue = event.target.value.toLowerCase();
+
+  let filterSelectRegion = countries.filter(
+    (country) => country.region.toLowerCase().includes(selectValue)
+  );
+
+  updateCountries(filterSelectRegion);
+
+})
